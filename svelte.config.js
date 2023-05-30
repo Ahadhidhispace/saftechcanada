1043d8e5174656105c72eab23505792ddd379233
Amani Amani Jonanathan <ahadhiamanijonathan@gmail.com>
2023-05-30 20:40:05 +0200

    unsetting adapter static add Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.includes('dev');
+// const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,16 +10,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-        }
+		adapter: adapter(
+			// {
+			// pages: 'build',
+			// assets: 'build',
+			// fallback: undefined,
+			// precompress: false,
+			// strict: true
+			// }
+		)
+		// ,
+		// paths: {
+        //     base: dev ? '' : process.env.BASE_PATH,
+        // }
 	}
 };
 
